72fc94c790ae926fdf18ff8f59d872b27474ec06
Alan Collins <alan.collins@bath.edu>
2025-05-12 18:51:36 -0400

    tidy dead code

--- Program.cs
+++ Program.cs
@@ -63,21 +63,6 @@ public class App
 
 
         return await rootCommand.InvokeAsync(args);
-
-        FileStream json = File.OpenRead(args[0]);
-        var profile = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(json);
-        var output = DistanceMatrix.FromJson(profile); //, distance: "normalized_allele_differences", max_tree_height: 0);
-
-
-        // test loading from a distance matrix
-        // string[] samples = output.Samples;
-        // float[,] dmat = output.Distmat;
-        // DistanceMatrix other = DistanceMatrix.FromDistanceMatrix(samples, dmat);
-
-        string tree = output.Tree;
-        Console.WriteLine(tree);
-
-        return 0;
     }
 
     private static string ParseDistance(string distance)
